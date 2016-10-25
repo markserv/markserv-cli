ace6c6de8b977effc292fa14b7a60627e2095215
F1LT3R <al@pwn.io>
2016-10-25 10:35:50 -0400

    Similfy plugin arch.

---
+++ core/plugin-arch.js
@@ -0,0 +1,12 @@
+const callerId = require('caller-id');
+const plugin = require('./plugin-modifier');
+
+module.exports = callback => {
+  const moduleId = callerId.getData().filePath;
+  const _module = require.cache[moduleId];
+
+  if (typeof callback === 'function') {
+    console.log('was obj');
+    plugin(_module, callback);
+  }
+};
--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
 global.log = require('./core/logging');
+
 const Markconf = require('./core/initialize-markconf.js');
-module.exports = require('./core/plugins.js');
+
+module.exports = require('./core/plugin-arch');
 
 const CLI = !module.parent;
 
 if (CLI) {
-  module.exports.markserv = require('./core/initialize-markserv.js')(Markconf);
+  const markserv = require('./core/initialize-markserv.js')(Markconf);
+  module.exports.markserv = markserv;
+  module.exports.helpers = require('./core/helpers');
 }
+
