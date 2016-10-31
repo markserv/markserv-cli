a8667be40c8050e6e0de327c68ac2b50c6b342db
F1LT3R <al@pwn.io>
2016-10-31 10:39:04 -0400

    Beter initialization.

--- index.js
+++ index.js
@@ -3,21 +3,15 @@
 // Prepare args for Markserv Configuration (Markconf)
 const args = require('./lib/init.args').parse(process.argv);
 
+// Markserv main service
+const Markserv = require('./lib/core.markserv');
+
+// Export Markserv for use in other apps
+module.exports = Markserv;
+
 const confResolver = require('./lib/conf-resolver');
 
-module.exports.plugin = require('./lib/core.plugin');
+const plugins = confResolver.resolveMarkconf(args.conf);
+const Markconf = require('./lib/init.markconf').initialize(args, plugins);
 
-const Markconf = confResolver.resolveMarkconf(args.conf);
-
-console.log(Markconf);
-
-// // Initialize Markconf to be consumed by Markserv
-// const Markconf = require('./lib/init.markconf').initialize(args);
-
-// // Markserv main service
-// const Markserv = require('./lib/core.markserv');
-
-// Markserv.spawnService(Markconf);
-
-// // Start Markserv service
-// module.exports = Markserv;
+Markserv.spawnService(Markconf);
