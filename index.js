4c2a88e75c9ba8839b9bb28e0718586e8739270d
F1LT3R <al@pwn.io>
2016-10-31 16:49:43 -0400

    Fix helper.

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
 
-// Prepare args for Markserv Configuration (Markconf)
-const args = require('./lib/init.args').parse(process.argv);
-
 // Markserv main service
 const Markserv = require('./lib/core.markserv');
 
 // Export Markserv for use in other apps
 module.exports = Markserv;
 
-const confResolver = require('./lib/conf-resolver');
+// const CLI = !module.parent;
 
-const plugins = confResolver.resolveMarkconf(args.conf);
+// // Run in client mode if appropriate
+// if (CLI) {
+const args = require('./lib/init.args').parse(process.argv);
+const plugins = require('./lib/plugin-resolver')(args.conf);
 const Markconf = require('./lib/init.markconf').initialize(args, plugins);
-
 Markserv.spawnService(Markconf);
+// }
