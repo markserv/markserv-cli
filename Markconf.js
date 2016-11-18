5513e75ab1e89ff4312277eb878ccb0d52b23c3f
F1LT3R <al@pwn.io>
2016-11-18 11:35:33 -0500

    Use clonedeep instead of obj assign.

--- Markconf.js
+++ Markconf.js
@@ -22,24 +22,24 @@ const Markconf = {
   // File includers
   // includers: 'markserv-contrib-app.github',
 
-  // includers: {
-    // html: 'markserv-contrib-inc.html',
+  includers: {
+    html: 'markserv-contrib-inc.html',
   //   // markdown: 'markserv-contrib-inc.markdown',
   //   // less: 'markserv-contrib-inc.less'
-  // },
+  },
 
   // HTTP Response Modifiers
-  modifiers: 'markserv-contrib-app.github'
-  // modifiers: {
-    // '**/': [
-      // {'markserv-contrib-mod.dir': 'partials/directory.html'},
-      // {'markserv-contrib-mod.foo': 'partials/directory.html'}
-    // ],
+  // modifiers: 'markserv-contrib-app.github'
+  modifiers: {
+    '**/': [
+      {'markserv-contrib-mod.dir': 'partials/directory.html'},
+    //   {'markserv-contrib-mod.foo': 'partials/directory.html'}
+    ],
     // '**/': 'markserv-contrib-mod.dir',
-  //   '**/*.md': 'markserv-contrib-mod.markdown',
-  //   '**/*': 'markserv-contrib-mod.file',
-  //   404: 'markserv-contrib-mod.http-404'
-  // }
+    // '**/*.md': 'markserv-contrib-mod.markdown',
+    // '**/*': 'markserv-contrib-mod.file',
+    // 404: 'markserv-contrib-mod.http-404'
+  }
 
     // core: 'markserv-contrib-app.github'
 
