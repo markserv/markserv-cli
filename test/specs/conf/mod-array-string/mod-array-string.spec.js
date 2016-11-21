88cddde454afcdc0ae6d5b17626cc6a8f900be1a
F1LT3R <al@pwn.io>
2016-11-21 13:49:31 -0500

    Add test to show modifier can be loaded in an array of strings.

---
+++ test/specs/conf/mod-array-string/mod-array-string.spec.js
@@ -0,0 +1,38 @@
+const chai = require('chai');
+
+const expect = chai.expect;
+
+const argv = [null, null,
+  // Use the Markconf file from this spec directory
+  '-c', __dirname,
+  // Turn off the logger
+  '-l', 'OFF'
+];
+
+describe('Markconf with modifier array containing strings', () => {
+  const markserv = require('app/markserv.js')(argv);
+  // console.log(markserv);
+  // console.log(markserv.Markconf.plugins.modifiers);
+
+  it('should initialize', () => {
+    expect(markserv.isInitialized).to.be.a('boolean');
+    expect(markserv.isInitialized).to.equal(true);
+  });
+
+  it('should have plugins modifiers object', () => {
+    expect(markserv.Markconf).to.be.an('object');
+    expect(markserv.Markconf.plugins).to.be.an('object');
+    expect(markserv.Markconf.plugins.modifiers).to.be.an('object');
+  });
+
+  it('should have a modifiers pattern array', () => {
+    expect(markserv.Markconf.plugins.modifiers['**/*.*']).to.be.an('array');
+    expect(markserv.Markconf.plugins.modifiers['**/*.*'].length).to.be.greaterThan(0);
+  });
+
+  it('should load the modifier: `markserv-contrib-mod.file`', () => {
+    const fileModifier = markserv.Markconf.plugins.modifiers['**/*.*'][0];
+    expect(fileModifier.name).to.equal('markserv-contrib-mod.file');
+    expect(fileModifier.httpResponseModifier).to.be.a('function');
+  });
+});
