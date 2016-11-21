e76de908d0ed16acf2c3f6f4971c10dc4226b6ca
F1LT3R <al@pwn.io>
2016-11-21 16:04:11 -0500

    Add test to check the loading of includes(though this part will likely change quiate a lot.

---
+++ test/specs/conf/inc-string/Markconf.js
@@ -0,0 +1,10 @@
+const Markconf = {
+  includers: {
+    html: 'markserv-contrib-inc.html'
+  },
+  modifiers: {
+    '**/*.*': 'markserv-contrib-mod.file'
+  }
+};
+
+module.exports = Markconf;
---
+++ test/specs/conf/inc-string/inc-string.spec.js
@@ -0,0 +1,42 @@
+const chai = require('chai');
+
+const expect = chai.expect;
+
+const argv = [null, null,
+  // Use the Markconf file from this spec directory
+  '-c', __dirname,
+  // Turn off the logger
+  '-l', 'OFF'
+];
+
+describe('Markconf with includer single string', () => {
+  it('should initialize with 1 includer (also requires 1 modifier)', done => {
+    const markserv = require('app/markserv.js')(argv);
+    // console.log(markserv);
+
+    // should initialize
+    expect(markserv.isInitialized).to.be.a('boolean');
+    expect(markserv.isInitialized).to.equal(true);
+    expect(markserv.Markconf).to.be.an('object');
+
+   // should have plugins includers object'
+    expect(markserv.Markconf.includers).to.be.an('object');
+    expect(markserv.Markconf.plugins.includers).to.be.an('object');
+
+   // should have plugins modifiers object'
+    expect(markserv.Markconf.modifiers).to.be.an('object');
+    expect(markserv.Markconf.plugins.modifiers).to.be.an('object');
+
+    // should have a includers html and modifiers ** pattern array
+    expect(markserv.Markconf.plugins.includers.html).to.be.an('object');
+    expect(markserv.Markconf.plugins.modifiers['**/*.*']).to.be.an('object');
+
+    // should load the modifier: `markserv-contrib-inc.html`
+    const htmlIncluder = markserv.Markconf.plugins.includers.html;
+    expect(htmlIncluder.name).to.equal('markserv-contrib-inc.html');
+    expect(htmlIncluder.htmlCommentIncluder).to.be.a('function');
+
+    markserv.kill();
+    done();
+  });
+});
