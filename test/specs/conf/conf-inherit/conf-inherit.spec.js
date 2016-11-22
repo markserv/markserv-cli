8a5ca5e198bd127ebf61f5c842faa2b889997119
F1LT3R <al@pwn.io>
2016-11-22 11:15:32 -0500

    Fixing path issues between tests and runtime.

--- test/specs/conf/conf-inherit/conf-inherit.spec.js
+++ test/specs/conf/conf-inherit/conf-inherit.spec.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const chai = require('chai');
 
 const expect = chai.expect;
@@ -6,21 +7,19 @@ const argv = [null, null,
   // Use the Markconf file from this spec directory
   '-c', __dirname,
   // Turn off the logger
-  '-l', 'OFF'
+  '-l', 'TRACE'
 ];
 
 describe('Markconf should be able to inherit from Github app', () => {
   it('should initialize with github app modifiers and includers', done => {
     const markserv = require('app/markserv.js')(argv);
-    console.log(markserv.Markconf);
 
     // should initialize
     expect(markserv.isInitialized).to.be.a('boolean');
     expect(markserv.isInitialized).to.equal(true);
     expect(markserv.Markconf).to.be.an('object');
 
-
-    console.log(markserv.Markconf.plugins.modifiers);
+    console.log(markserv.Markconf.plugins);
 
     // should have plugins includers object
     // expect(markserv.Markconf.includers).to.be.an('object');
