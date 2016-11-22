5139cb3fdd359a55ad840855c60250309ddf3bb1
F1LT3R <al@pwn.io>
2016-11-22 11:24:11 -0500

    Fix inherit test.

--- test/specs/conf/conf-inherit/conf-inherit.spec.js
+++ test/specs/conf/conf-inherit/conf-inherit.spec.js
@@ -7,28 +7,36 @@ const argv = [null, null,
   // Use the Markconf file from this spec directory
   '-c', __dirname,
   // Turn off the logger
-  '-l', 'TRACE'
+  '-l', 'OFF'
 ];
 
 describe('Markconf should be able to inherit from Github app', () => {
   it('should initialize with github app modifiers and includers', done => {
     const markserv = require('app/markserv.js')(argv);
+    // console.log(markserv.Markconf.plugins);
 
     // should initialize
     expect(markserv.isInitialized).to.be.a('boolean');
     expect(markserv.isInitialized).to.equal(true);
     expect(markserv.Markconf).to.be.an('object');
 
-    console.log(markserv.Markconf.plugins);
-
     // should have plugins includers object
-    // expect(markserv.Markconf.includers).to.be.an('object');
-    // expect(markserv.Markconf.plugins.includers).to.be.an('object');
+    expect(markserv.Markconf.plugins).to.be.an('object');
+    expect(markserv.Markconf.plugins.includers).to.be.an('object');
 
     // should have plugins modifiers object
-    // expect(markserv.Markconf.modifiers).to.be.an('object');
-    // expect(markserv.Markconf.plugins.modifiers).to.be.an('object');
+    expect(markserv.Markconf.plugins).to.be.an('object');
+    expect(markserv.Markconf.plugins.modifiers).to.be.an('object');
 
+    // should find a 404 modifier
+    expect(markserv.Markconf.plugins.modifiers['404']).to.be.an('object');
+    expect(markserv.Markconf.plugins.modifiers['404'].name).to.equal('markserv-contrib-mod.http-404');
+    expect(markserv.Markconf.plugins.modifiers['404'].httpResponseModifier).to.be.a('function');
+
+    // should find an html indluder
+    expect(markserv.Markconf.plugins.includers.html).to.be.an('object');
+    expect(markserv.Markconf.plugins.includers.html.name).to.equal('markserv-contrib-inc.html');
+    expect(markserv.Markconf.plugins.includers.html.htmlCommentIncluder).to.be.a('function');
     markserv.kill();
 
     done();
