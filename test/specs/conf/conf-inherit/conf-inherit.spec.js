fffaf1bfd4c46a69a5a8d400a109eeab07cecfff
F1LT3R <al@pwn.io>
2016-11-24 01:15:03 -0500

    Promisify incluer loading.

--- test/specs/conf/conf-inherit/conf-inherit.spec.js
+++ test/specs/conf/conf-inherit/conf-inherit.spec.js
@@ -7,32 +7,32 @@ const argv = [null, null,
   // Use the Markconf file from this spec directory
   '-c', __dirname,
   // Turn off the logger
-  '-l', 'trace'
-  // '-l', 'OFF'
+  // '-l', 'trace'
+  '-l', 'OFF'
 ];
 
 describe('Markconf should be able to inherit from Github app', () => {
   it('should initialize with github app modifiers and includers', done => {
     require('app/markserv.js')(argv).then(markserv => {
-      console.log(markserv);
+      // console.log(markserv);
 
-      // // should initialize
-      // expect(markserv.isInitialized).to.be.a('boolean');
-      // expect(markserv.isInitialized).to.equal(true);
-      // expect(markserv.Markconf).to.be.an('object');
+      // should initialize
+      expect(markserv.isInitialized).to.be.a('boolean');
+      expect(markserv.isInitialized).to.equal(true);
+      expect(markserv.Markconf).to.be.an('object');
 
-      // // should have plugins includers object
+      // should have plugins includers object
       // expect(markserv.Markconf.plugins).to.be.an('object');
       // expect(markserv.Markconf.plugins.includers).to.be.an('object');
 
-      // // should have plugins modifiers object
-      // expect(markserv.Markconf.plugins).to.be.an('object');
-      // expect(markserv.Markconf.plugins.modifiers).to.be.an('object');
+      // should have plugins modifiers object
+      expect(markserv.Markconf.plugins).to.be.an('object');
+      expect(markserv.Markconf.plugins.modifiers).to.be.an('object');
 
-      // // should find a 404 modifier
-      // expect(markserv.Markconf.plugins.modifiers['404']).to.be.an('object');
-      // expect(markserv.Markconf.plugins.modifiers['404'].name).to.equal('markserv-contrib-mod.http-404');
-      // expect(markserv.Markconf.plugins.modifiers['404'].httpResponseModifier).to.be.a('function');
+      // should find a 404 modifier
+      expect(markserv.Markconf.plugins.modifiers['404']).to.be.an('object');
+      expect(markserv.Markconf.plugins.modifiers['404'].name).to.equal('markserv-contrib-mod.http-404');
+      expect(markserv.Markconf.plugins.modifiers['404'].httpResponseModifier).to.be.a('function');
 
       // // should find an html indluder
       // expect(markserv.Markconf.plugins.includers.html).to.be.an('object');
