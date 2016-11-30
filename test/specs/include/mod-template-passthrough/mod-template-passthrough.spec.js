d8b0c770c23c158663d59b52c893c4a2faff2f62
F1LT3R <al@pwn.io>
2016-11-30 13:51:24 -0500

    Fixed bad promise stack in modifier template compilation steps.

--- test/specs/include/mod-template-passthrough/mod-template-passthrough.spec.js
+++ test/specs/include/mod-template-passthrough/mod-template-passthrough.spec.js
@@ -10,44 +10,39 @@ const argv = [null, null,
   // Use the Markconf file from this spec directory
   '-c', __dirname,
   // Turn off the logger
-  // '-l', 'trace'
   // '-p', '8889',
-  '-l', 'OFF',
-  '-r', path.join(__dirname, 'partials')
+  '-r', path.join(__dirname, 'partials'),
+  // '-l', 'trace'
+  '-l', 'OFF'
 ];
 
 describe('Markconf with HTML includer', () => {
-  it('should be able to nest HTML levels deep', function(done) {
-    // Sometimes we can try and get the http request before the compiler has
-    // finished, which wouldn't happen in ptoduction, so we add a little delay
-    this.timeout(5000);
+  it('should be able to nest HTML levels deep', done => {
 
     require('app/markserv')(argv).then(markserv => {
       // console.log(markserv.Markconf);
 
-      setTimeout(() => {
-        // should initialize
-        expect(markserv.isInitialized).to.be.a('boolean');
-        expect(markserv.isInitialized).to.equal(true);
-        expect(markserv.Markconf).to.be.an('object');
+      // should initialize
+      expect(markserv.isInitialized).to.be.a('boolean');
+      expect(markserv.isInitialized).to.equal(true);
+      expect(markserv.Markconf).to.be.an('object');
 
-        const expectedHtml = fs.readFileSync(path.join(__dirname, 'expected.html'), 'utf8');
+      const expectedHtml = fs.readFileSync(path.join(__dirname, 'expected.html'), 'utf8');
 
-        http.get({
-          port: markserv.Markconf.port,
-          headers: {'Cache-Control': 'no-cache'}
-        }).on('response', res => {
-          res.setEncoding('utf8');
-          res.on('data', data => {
-            // console.log(data);
-            expect(data).to.equal(expectedHtml);
+      http.get({
+        port: markserv.Markconf.port,
+        headers: {'Cache-Control': 'no-cache'}
+      }).on('response', res => {
+        res.setEncoding('utf8');
+        res.on('data', data => {
+          // console.log(data);
+          expect(data).to.equal(expectedHtml);
 
-            markserv.kill();
+          markserv.kill();
 
-            done();
-          });
+          done();
         });
-      }, 1000);
+      });
     });
   });
 });
