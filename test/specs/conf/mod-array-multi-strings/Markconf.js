55df5cf819cf1532a2f54ab1c96e97c998c6e521
F1LT3R <al@pwn.io>
2016-12-05 17:28:23 -0500

    Fix another test.

--- test/specs/conf/mod-array-multi-strings/Markconf.js
+++ test/specs/conf/mod-array-multi-strings/Markconf.js
@@ -1,12 +1,12 @@
 const Markconf = {
-  modifiers: {
-    '**/*.*': [
-      // These two mods should not be used together.
-      // We are just showing that we can load multiple mods into the stack.
-      'markserv-contrib-mod.file',
-      'markserv-contrib-mod.dir'
-    ]
-  }
+	modifiers: {
+		'**/*.*': [
+			// These two mods should not be used together.
+			// We are just showing that we can load multiple mods into the stack.
+			'markserv-contrib-mod.file',
+			'markserv-contrib-mod.dir'
+		]
+	}
 };
 
 module.exports = Markconf;
--- test/specs/conf/mod-array-multi-strings/mod-array-multi-strings.spec.js
+++ test/specs/conf/mod-array-multi-strings/mod-array-multi-strings.spec.js
@@ -3,43 +3,42 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const argv = [null, null,
-  // Use the Markconf file from this spec directory
-  '-c', __dirname,
-  // Turn off the logger
-  '-l', 'ERROR'
+	// Use the Markconf file from this spec directory
+	'-c', __dirname,
+	// Turn off the logger
+	'-l', 'ERROR'
 ];
 
 describe('Markconf with modifier array containing strings', () => {
-  it('should initialize', done => {
-    require('app/markserv')(argv).then(markserv => {
-      // console.log(markserv);
+	it('should initialize', done => {
+		require('app/markserv')(argv).then(markserv => {
+			// console.log(markserv);
 
-      // should initialize
-      expect(markserv.isInitialized).to.be.a('boolean');
-      expect(markserv.isInitialized).to.equal(true);
+			// should initialize
+			expect(markserv.isInitialized).to.be.a('boolean');
+			expect(markserv.isInitialized).to.equal(true);
 
-      // should have plugins modifiers object
-      expect(markserv.Markconf).to.be.an('object');
-      expect(markserv.Markconf.plugins).to.be.an('object');
-      expect(markserv.Markconf.plugins.modifiers).to.be.an('object');
+			// should have plugins modifiers object
+			expect(markserv.plugins).to.be.an('object');
+			expect(markserv.plugins.modifiers).to.be.an('object');
 
-      // should have a modifiers pattern array
-      expect(markserv.Markconf.plugins.modifiers['**/*.*']).to.be.an('array');
-      expect(markserv.Markconf.plugins.modifiers['**/*.*'].length).to.be.greaterThan(1);
+			// should have a modifiers pattern array
+			expect(markserv.plugins.modifiers['**/*.*']).to.be.an('array');
+			expect(markserv.plugins.modifiers['**/*.*'].length).to.be.greaterThan(1);
 
-      // should load the modifier: `markserv-contrib-mod.file`
-      const fileModifierA = markserv.Markconf.plugins.modifiers['**/*.*'][0];
-      expect(fileModifierA.name).to.equal('markserv-contrib-mod.file');
-      expect(fileModifierA.httpResponseModifier).to.be.a('function');
+			// should load the modifier: `markserv-contrib-mod.file`
+			const fileModifierA = markserv.plugins.modifiers['**/*.*'][0];
+			expect(fileModifierA.name).to.equal('markserv-contrib-mod.file');
+			expect(fileModifierA.httpResponseModifier).to.be.a('function');
 
-      // should load the modifier: `markserv-contrib-mod.dir`
-      const fileModifierB = markserv.Markconf.plugins.modifiers['**/*.*'][1];
-      expect(fileModifierB.name).to.equal('markserv-contrib-mod.dir');
-      expect(fileModifierB.httpResponseModifier).to.be.a('function');
+			// should load the modifier: `markserv-contrib-mod.dir`
+			const fileModifierB = markserv.plugins.modifiers['**/*.*'][1];
+			expect(fileModifierB.name).to.equal('markserv-contrib-mod.dir');
+			expect(fileModifierB.httpResponseModifier).to.be.a('function');
 
-      markserv.kill();
+			markserv.kill(markserv);
 
-      done();
-    });
-  });
+			done();
+		});
+	});
 });
