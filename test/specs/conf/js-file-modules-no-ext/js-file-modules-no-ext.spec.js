05d08a5e6f9fbfdab35dda975dda3f65a388e75c
F1LT3R <al@pwn.io>
2016-12-05 16:53:59 -0500

    Fix js string without extension test.

--- test/specs/conf/js-file-modules-no-ext/js-file-modules-no-ext.spec.js
+++ test/specs/conf/js-file-modules-no-ext/js-file-modules-no-ext.spec.js
@@ -3,47 +3,45 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const argv = [null, null,
-  // Use the Markconf file from this spec directory
-  '-c', __dirname,
-  // Turn off the logger
-  '-l', 'OFF'
-  // '-l', 'TRACE'
+	// Use the Markconf file from this spec directory
+	'-c', __dirname,
+	// Turn off the logger
+	'-l', 'OFF'
+	// '-l', 'TRACE'
 ];
 
 describe('Markconf using js strings (without extensions)', () => {
-  it('should initialize with 1 includer and 1 modifier)', done => {
-    require('app/markserv')(argv).then(markserv => {
-      // console.log(markserv);
+	it('should initialize with 1 includer and 1 modifier)', done => {
+		require('app/markserv')(argv).then(markserv => {
+			// console.log(markserv);
 
-      // should initialize
-      expect(markserv.isInitialized).to.be.a('boolean');
-      expect(markserv.isInitialized).to.equal(true);
-      expect(markserv.Markconf).to.be.an('object');
+			// should initialize
+			expect(markserv.isInitialized).to.be.a('boolean');
+			expect(markserv.isInitialized).to.equal(true);
+			expect(markserv.Markconf).to.be.an('object');
 
-     // should have plugins includers object'
-      expect(markserv.Markconf.includers).to.be.an('object');
-      expect(markserv.Markconf.plugins.includers).to.be.an('object');
+			// should have plugins includers object'
+			expect(markserv.plugins.includers).to.be.an('object');
 
-     // should have plugins modifiers object'
-      expect(markserv.Markconf.modifiers).to.be.an('object');
-      expect(markserv.Markconf.plugins.modifiers).to.be.an('object');
+			// should have plugins modifiers object'
+			expect(markserv.plugins.modifiers).to.be.an('object');
 
-      // should have a includers html and modifiers ** pattern array
-      expect(markserv.Markconf.plugins.includers.html).to.be.an('object');
-      expect(markserv.Markconf.plugins.modifiers['**/*.*']).to.be.an('object');
+			// should have a includers html and modifiers ** pattern array
+			expect(markserv.plugins.includers.html).to.be.an('object');
+			expect(markserv.plugins.modifiers['**/*.*']).to.be.an('object');
 
-      // should load the modifier: `markserv-contrib-inc.html`
-      const htmlIncluder = markserv.Markconf.plugins.includers.html;
-      expect(htmlIncluder.name).to.equal('html');
-      expect(htmlIncluder.htmlCommentIncluder).to.be.a('function');
+			// should load the modifier: `markserv-contrib-inc.html`
+			const htmlIncluder = markserv.plugins.includers.html;
+			expect(htmlIncluder.name).to.equal('custom-includer');
+			expect(htmlIncluder.htmlCommentIncluder).to.be.a('function');
 
-      const fileModifier = markserv.Markconf.plugins.modifiers['**/*.*'];
-      expect(fileModifier.name).to.equal('**/*.*');
-      expect(fileModifier.httpResponseModifier).to.be.a('function');
+			const fileModifier = markserv.plugins.modifiers['**/*.*'];
+			expect(fileModifier.name).to.equal('custom-modifier');
+			expect(fileModifier.httpResponseModifier).to.be.a('function');
 
-      markserv.kill();
+			markserv.kill(markserv);
 
-      done();
-    });
-  });
+			done();
+		});
+	});
 });
