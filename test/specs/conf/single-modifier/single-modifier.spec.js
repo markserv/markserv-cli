50a9c081cc3e3630c503fe76d18d2fa63d9c134f
F1LT3R <al@pwn.io>
2016-12-12 10:59:57 -0500

    Fix single modifier test.

--- test/specs/conf/single-modifier/single-modifier.spec.js
+++ test/specs/conf/single-modifier/single-modifier.spec.js
@@ -5,7 +5,7 @@ const expect = chai.expect;
 const argv = [null, null,
 	// Use the Markconf file from this spec directory
 	'-c', __dirname,
-	// Turn off the logger
+	// Turn off the logger for testing
 	'-l', 'OFF'
 ];
 
@@ -14,9 +14,9 @@ describe('single modifier', () => {
 		require('app/markserv')(argv).then(markserv => {
 			// console.log(markserv);
 
-			expect(markserv.isInitialized).to.be.a('boolean');
-			expect(markserv.isInitialized).to.equal(true);
-			markserv.kill(markserv);
+			expect(markserv.initialized).to.be.a('boolean');
+			expect(markserv.initialized).to.equal(true);
+			markserv.shutdown(markserv);
 
 			done();
 		});
