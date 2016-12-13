dec20457ddb918500e44719780a3044960f9e3e1
F1LT3R <al@pwn.io>
2016-12-13 10:46:02 -0500

    Use not scripts instead of bin scripts.

--- Markconf.js
+++ Markconf.js
@@ -1,45 +1,32 @@
-const Markconf = {
-	// title: 'Markconf Example',
+// const Markconf = {
 
-	// serverRoot: 'some-pre-defined-path',
+// 	// watch: {
+// 	//   Markconf: true,
+// 	//   modifiers: true,
+// 	//   includers: true,
+// 	//   templates: true,
+// 	//   paths: true
+// 	// },
 
-	// defaults: {
-	//   fileTypes: {
-	//     markdown: [
-	//      '.md'
-	//    ]
-	//   }
-	// }
+// 	includers: {
+// 		html: 'markserv-contrib-inc.html',
+// 		markdown: 'markserv-contrib-inc.markdown',
+// 		// less: 'markserv-contrib-inc.less'
+// 	},
 
-	// watch: {
-	//   Markconf: true,
-	//   modifiers: true,
-	//   includers: true,
-	//   templates: true,
-	//   paths: true
-	// },
+// 	modifiers: {
+// 		// '**/': {
+// 			// module: 'markserv-contrib-mod.dir',
+// 			// template: 'test/partials/test.html'
+// 		// },
+// 		'**/': 'markserv-contrib-mod.dir',
+// 		'**/*.html': 'markserv-contrib-mod.html',
+// 		'**/*.md': 'markserv-contrib-mod.markdown',
+// 		'**/*': 'markserv-contrib-mod.file',
+// 		404: 'markserv-contrib-mod.http-404'
+// 	}
+// };
+//
+// module.exports = Markconf;
 
-	includers: {
-		html: 'markserv-contrib-inc.html',
-		markdown: 'markserv-contrib-inc.markdown',
-		// less: 'markserv-contrib-inc.less'
-	},
-
-	modifiers: {
-		// '**/': {
-			// module: 'markserv-contrib-mod.dir',
-			// template: 'test/partials/test.html'
-		// },
-		'**/': 'markserv-contrib-mod.dir',
-		'**/*.html': 'markserv-contrib-mod.html',
-		'**/*.md': 'markserv-contrib-mod.markdown',
-		'**/*': 'markserv-contrib-mod.file',
-		404: 'markserv-contrib-mod.http-404'
-	}
-
-	// rewrites: {
-	// 	'test/redirect-a/**/*': 'test/redirect-b/**/*'
-	// }
-};
-
-module.exports = Markconf;
+module.exports = require('markserv-contrib-app.github');
--- bin/teardown
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rm -r node_modules
