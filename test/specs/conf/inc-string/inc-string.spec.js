7167eb923561d4ffd33305397396ac49b8c0a887
F1LT3R <al@pwn.io>
2016-12-13 11:22:22 -0500

    Remove logging statement.

--- test/specs/conf/inc-string/inc-string.spec.js
+++ test/specs/conf/inc-string/inc-string.spec.js
@@ -13,7 +13,7 @@ const argv = [null, null,
 describe('Markconf with includer single string', () => {
 	it('should initialize with 1 includer (also requires 1 modifier)', done => {
 		require('app/markserv')(argv).then(markserv => {
-			console.log(markserv);
+			// console.log(markserv);
 
 			// // should initialize
 			expect(markserv.initialized).to.be.a('boolean');
