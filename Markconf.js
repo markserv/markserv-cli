88dedb8d8712a325197f946ddb0bf4224647605a
F1LT3R <al@pwn.io>
2016-12-14 09:53:59 -0500

    Quick save.

--- Markconf.js
+++ Markconf.js
@@ -1,22 +1,24 @@
 const Markconf = {
 	import: 'markserv-contrib-app.github',
 
-	includers: {
-		html: 'markserv-contrib-inc.html',
+	// includers: {
+	// 	html: 'markserv-contrib-inc.html',
 	// 	markdown: 'markserv-contrib-inc.markdown',
-	// 	less: 'markserv-contrib-inc.less'
-	},
+	// // 	less: 'markserv-contrib-inc.less'
+	// },
 
 	modifiers: {
-		'**/': {
-			module: '@import',
-			templateUrl: 'partials/test.html'
-		},
-		// '**/*.html': 'markserv-contrib-mod.html',
-		// '**/*.md': 'markserv-contrib-mod.markdown',
-		// '**/*': 'markserv-contrib-mod.file',
-		// 404: 'markserv-contrib-mod.http-404'
-	}
+		// '**/': {
+		// 	module: '@import',
+		// 	templateUrl: 'partials/test.html'
+		// },
+		// '**': 'markserv-contrib-mod.dir',
+
+	// 	// '**/*.html': 'markserv-contrib-mod.html',
+	// 	// '**/*.md': 'markserv-contrib-mod.markdown',
+	// 	// '**/*': 'markserv-contrib-mod.file',
+	// 	// 404: 'markserv-contrib-mod.http-404'
+	// }
 
 	// watch: {
 	//   Markconf: true,
@@ -24,7 +26,7 @@ const Markconf = {
 	//   includers: true,
 	//   templates: true,
 	//   paths: true
-	// },
+	},
 };
 
 module.exports = Markconf;
