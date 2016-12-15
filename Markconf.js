4d067ac954a91295957c8b60b569d942544b9fa0
F1LT3R <al@pwn.io>
2016-12-15 10:18:52 -0500

    Add detailed trace for resolver and plugins.

--- Markconf.js
+++ Markconf.js
@@ -2,23 +2,22 @@ const Markconf = {
 	import: 'markserv-contrib-app.github',
 
 	includers: {
-	// 	html: 'markserv-contrib-inc.html',
+		// html: 'markserv-contrib-inc.html',
 		markdown: 'markserv-contrib-inc.markdown',
-	// // 	less: 'markserv-contrib-inc.less'
+		// less: 'markserv-contrib-inc.less'
 	},
 
 	modifiers: {
-		// '**/': {
-		// 	module: '@import',
-		// 	templateUrl: 'partials/test.html'
-		// },
-		// '**': 'markserv-contrib-mod.dir',
-
-	// 	// '**/*.html': 'markserv-contrib-mod.html',
-	// 	// '**/*.md': 'markserv-contrib-mod.markdown',
-	// 	// '**/*': 'markserv-contrib-mod.file',
-	// 	// 404: 'markserv-contrib-mod.http-404'
-	// }
+		'**/': {
+			module: '@import',
+			// templateUrl: 'examples/includes/test/test.html'
+		},
+		// '**/': 'markserv-contrib-mod.dir',
+		'**/*.html': 'markserv-contrib-mod.html',
+		'**/*.md': 'markserv-contrib-mod.markdown',
+		'**/*': 'markserv-contrib-mod.file',
+		404: 'markserv-contrib-mod.http-404'
+	}
 
 	// watch: {
 	//   Markconf: true,
@@ -26,7 +25,7 @@ const Markconf = {
 	//   includers: true,
 	//   templates: true,
 	//   paths: true
-	},
+	// },
 };
 
 module.exports = Markconf;
--- doc/examples/includes/markdown-in-markdown/include.md
+++ doc/examples/includes/markdown-in-markdown/include.md
@@ -1,1 +1,3 @@
 <div style="border: 1px solid black; padding: 20px; margin: 20px; background: green;">This is [include.md](include.md).</div>
+
+<!--markserv|markdown|something.md-->
