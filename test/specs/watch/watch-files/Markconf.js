9d4c0b0e5a3b85c70bdfde2792fdd903e77b083a
F1LT3R <al@pwn.io>
2017-02-20 03:28:06 -0500

    Argh it's to do with the subconf that browsersync is not working.

---
+++ test/specs/watch/watch-files/Markconf.js
@@ -0,0 +1,16 @@
+const Markconf = {
+
+			import: 'markserv-contrib-app.github',
+	// modifiers: {
+	// 	'**/': 'markserv-contrib-mod.dir',
+	// 	'**/*.md': 'markserv-contrib-mod.markdown'
+	// },
+
+	watch: {
+		files: [
+			'**/*.md'
+		]
+	}
+};
+
+module.exports = Markconf;
